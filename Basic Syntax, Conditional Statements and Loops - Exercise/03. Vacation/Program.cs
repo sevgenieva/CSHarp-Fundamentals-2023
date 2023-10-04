35bde9d28cd90c19da37523374b02234ad1edac0
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-04 22:17:04 +0100

    03._Vacation

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/03. Vacation/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace Exercise
+
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string typeOfGroup = Console.ReadLine();
+            string dayOfTheWeek = Console.ReadLine();
+
+            double price = 0;
+
+            if (typeOfGroup == "Students")
+            {
+                if (dayOfTheWeek == "Friday")
+                {
+                    price = people * 8.45;
+                }
+                else if (dayOfTheWeek == "Saturday")
+                {
+                    price = people * 9.80;
+                }
+                else if (dayOfTheWeek == "Sunday")
+                {
+                    price = people * 10.46;
+                }
+
+                if (people >= 30)
+                {
+                    price -= price * 0.15;
+                }
+            }
+            else if (typeOfGroup == "Business")
+            {
+                if (dayOfTheWeek == "Friday")
+                {
+                    price = people * 10.90;
+
+                    if (people >= 100)
+                    {
+                        price = (people - 10) * 10.90;
+                    }
+                }
+                else if (dayOfTheWeek == "Saturday")
+                {
+                    price = people * 15.60;
+
+                    if (people >= 100)
+                    {
+                        price = (people - 10) * 15.60;
+                    }
+                }
+                else if (dayOfTheWeek == "Sunday")
+                {
+                    price = people * 16;
+
+
+                    if (people >= 100)
+                    {
+                        price = (people - 10) * 16;
+                    }
+                }
+
+            }
+            else if (typeOfGroup == "Regular")
+            {
+                if (dayOfTheWeek == "Friday")
+                {
+                    price = people * 15;
+                }
+                else if (dayOfTheWeek == "Saturday")
+                {
+                    price = people * 20;
+                }
+                else if (dayOfTheWeek == "Sunday")
+                {
+                    price = people * 22.50;
+                }
+
+                if (people >= 10 && people <= 20)
+                {
+                    price -= price * 0.05;
+                }
+
+            }
+
+            Console.WriteLine($"Total price: {price:f2}");
+
+
+
+
+
+
+
+
+        }
+    }
+}
