0caf7008923a5755a9c29e1168a5704aad0fd4b2
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-04 21:48:59 +0100

    12._Even_Number

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/12. Even Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _12._Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            while (Math.Abs(number = int.Parse(Console.ReadLine())) % 2 == 1)
+            {
+                Console.WriteLine("Please write an even number.");
+
+
+            }
+            Console.WriteLine($"The number is: {Math.Abs(number)}");
+
+
+        }
+    }
+}
