652ae541292d3f1ee7b9212bc7376ce222da6a53
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-04 21:48:07 +0100

    8._Divisible_by_3

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/08. Divisible by 3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _8._Divisible_by_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 99; i += 3)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
