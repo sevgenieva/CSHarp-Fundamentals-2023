51614e33deea56ab043050aba12d0f6300d38836
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-04 15:22:37 +0100

    Problem Solving

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/03. Passed or Failed/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03._Passed_or_Failed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
