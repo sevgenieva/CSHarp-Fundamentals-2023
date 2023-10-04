1589b8462f56ade9df7882407e62a025fc64012d
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-04 21:48:26 +0100

    10._Multiplication_Table

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/10. Multiplication Table/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _10._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int sum = number * i;
+                Console.WriteLine($"{number} X {i} = {sum}");
+            }
+        }
+    }
+}
