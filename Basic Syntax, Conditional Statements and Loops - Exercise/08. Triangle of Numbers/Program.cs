c9aa71b1f77b3e5fe77664a2857e3fcf92be957c
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-04 22:18:09 +0100

    08._Triangle_of_Numbers

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/08. Triangle of Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write($"{i} ");
+                }
+            }
+
+
+        }
+    }
+}
