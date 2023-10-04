2c2d0da5d6dd9c21d9da6f261240608520c40ff1
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-04 21:48:16 +0100

    9._Sum_of_Odd_Numbers

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _9._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+
+
+
+                oddSum = ((2 * i) - 1);
+                Console.WriteLine(oddSum);
+                sum += oddSum;
+
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
