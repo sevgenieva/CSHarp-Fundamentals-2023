e749598667af1e3be435b2eb69f359eaddcb85bf
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 13:41:49 +0100

    02.__Pounds_to_Dollars

---
+++ Data Types and Variables - Lab/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+
+            double convert = (pounds * 1.31);
+
+            Console.WriteLine($"{convert:f3}");
+
+        }
+    }
+}
