8d8910c495ff946cdb56f5660dcb058e2e347d71
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 13:42:28 +0100

    06._Reversed_Chars

---
+++ Data Types and Variables - Lab/06. Reversed Chars/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+            char letter1 = char.Parse(Console.ReadLine());
+            char letter2 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{letter2} {letter1} {letter}");
+
+        }
+    }
+}
