161352ffafa95f306599f4249748fb3264535edf
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 13:42:38 +0100

    07._Concat_Names

---
+++ Data Types and Variables - Lab/07. Concat Names/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace Lab
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{name}{delimiter}{name2}");
+        }
+    }
+}
