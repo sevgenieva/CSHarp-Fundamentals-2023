7890926e2a725a63fcd60b1094ab6f0ffe7ee7c2
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 13:13:07 +0100

    04._Reverse_String

---
+++ Basic Syntax, Conditional Statements and Loops - More Exercise/04. Reverse String/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MoreExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                Console.Write(text[i]);
+            }
+        }
+    }
+}
