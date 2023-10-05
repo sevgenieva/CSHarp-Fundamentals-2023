8113d4b313409372e90baceaaff08fcee5180ca4
sevgenieva <126795272+sevgenieva@users.noreply.github.com>
2023-10-05 13:43:04 +0100

    10._Lower_or_Upper

---
+++ Data Types and Variables - Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if (letter >= 65 && letter <= 90)
+            {
+
+                Console.WriteLine("upper-case");
+
+
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
